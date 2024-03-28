739578874941dbb78aab2e033522c312660f29d1
Wsinthechat2912 <k2125891@kingston.ac.uk>
2024-03-28 21:24:04 +0000

    datascreen update
    still need to fix the graph

--- managementApp/screens/DataScreen.js
+++ managementApp/screens/DataScreen.js
@@ -1,42 +1,53 @@
-import React, { useContext, useEffect, useState } from "react";
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
-import { PieChart } from "react-native-chart-kit";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Dimensions,
+  SafeAreaView,
+  StyleSheet,
+} from "react-native";
+import { PieChart, BarChart } from "react-native-chart-kit";
 import { useUser } from "../data/UserContext";
-import { Dimensions } from "react-native";
 
 const screenWidth = Dimensions.get("window").width;
 
 const DataScreen = () => {
   const { user } = useUser();
+  const [selectedChart, setSelectedChart] = useState("pie");
   const [pieChartData, setPieChartData] = useState([]);
+  const [barChartData, setBarChartData] = useState({
+    labels: [],
+    datasets: [{ data: [] }],
+  });
+  const [upperLimit, setUpperLimit] = useState(0);
 
   useEffect(() => {
     if (user?.expenditures) {
       const expensesByCategory = user.expenditures.reduce((acc, expense) => {
-        const amountAsString = String(expense.amount);
-        const amount = parseFloat(amountAsString.replace("£", ""));
-        if (acc[expense.category]) {
-          acc[expense.category] += amount;
-        } else {
-          acc[expense.category] = amount;
-        }
+        const amount = parseFloat(expense.amount.replace("£", ""));
+        acc[expense.category] = (acc[expense.category] || 0) + amount;
         return acc;
       }, {});
-  
-      const chartData = Object.entries(expensesByCategory).map(
-        ([category, amount]) => ({
-          name: category,
-          population: amount,
-          color: getColorForCategory(category),
-          legendFontColor: "#7F7F7F",
-          legendFontSize: 15,
-        })
-      );
-  
-      setPieChartData(chartData);
+
+      const pieData = Object.keys(expensesByCategory).map((category) => ({
+        name: category,
+        population: expensesByCategory[category],
+        color: getColorForCategory(category),
+        legendFontColor: "#7F7F7F",
+        legendFontSize: 15,
+      }));
+
+      setPieChartData(pieData);
+
+      const labels = Object.keys(expensesByCategory);
+      const data = Object.values(expensesByCategory);
+      setBarChartData({ labels, datasets: [{ data }] });
+
+      const maxExpense = Math.max(...data);
+      setUpperLimit(Math.ceil((maxExpense + 5) / 10) * 10);
     }
   }, [user?.expenditures]);
-
 
   const getColorForCategory = (category) => {
     const categoryColors = {
@@ -46,42 +57,90 @@ const DataScreen = () => {
       Entertainment: "#581845",
       Other: "#FFC300",
     };
-    const color = categoryColors[category] || "#999999";
-    console.log(`Color for ${category}: ${color}`); // Add this line for debugging
-    return color;
+    return categoryColors[category] || "#999999";
   };
 
   const chartConfig = {
-    backgroundGradientFrom: "#fff",
-    backgroundGradientTo: "#fff",
-    color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+    backgroundGradientFrom: "#ffffff",
+    backgroundGradientTo: "#ffffff",
+    decimalPlaces: 2,
+    color: (opacity = 1) => `rgba(78, 156, 175, ${opacity})`,
     labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-    style: {
-      borderRadius: 16,
-    },
-    propsForDots: {
-      r: "6",
-      strokeWidth: "2",
-      stroke: "#ffa726",
-    },
+    barPercentage: 0.7,
+    useShadowColorFromDataset: false,
+  };
+
+  const toggleChart = (chartType) => {
+    setSelectedChart(chartType);
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.header}>Expenses Overview</Text>
+      <Text style={styles.header}>Expenses Overview</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={() => toggleChart("pie")}
+          style={[
+            styles.button,
+            selectedChart === "pie"
+              ? styles.activeButton
+              : styles.inactiveButton,
+          ]}
+        >
+          <Text style={styles.buttonText}>Pie Chart</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => toggleChart("bar")}
+          style={[
+            styles.button,
+            selectedChart === "bar"
+              ? styles.activeButton
+              : styles.inactiveButton,
+          ]}
+        >
+          <Text style={styles.buttonText}>Bar Chart</Text>
+        </TouchableOpacity>
       </View>
-      <PieChart
-        data={pieChartData}
-        width={screenWidth}
-        height={220}
-        chartConfig={chartConfig}
-        accessor={"population"}
-        backgroundColor={"transparent"}
-        paddingLeft={"15"}
-        center={[10, 10]}
-        absolute
-      />
+      <View style={styles.chartContainer}>
+        {selectedChart === "pie" && pieChartData.length > 0 ? (
+          <PieChart
+            data={pieChartData}
+            width={screenWidth - 32}
+            height={220}
+            chartConfig={chartConfig}
+            accessor={"population"}
+            backgroundColor={"transparent"}
+            paddingLeft={"15"}
+            center={[screenWidth / 4, 10]}
+            avoidFalseZero={true}
+            renderLegend={(legend) => (
+              <View style={styles.legend}>
+                {legend.map((item, index) => (
+                  <Text key={index} style={styles.legendText}>
+                    {`${item.name}: ${item.population}`}
+                  </Text>
+                ))}
+              </View>
+            )}
+          />
+        ) : selectedChart === "bar" && barChartData.labels.length > 0 ? (
+          <BarChart
+            data={barChartData}
+            width={screenWidth - 32}
+            height={220}
+            yAxisLabel={"£"}
+            chartConfig={chartConfig}
+            verticalLabelRotation={30}
+            fromZero
+            yAxisInterval={1}
+            yLabelsOffset={10}
+            yAxisRange={[0, upperLimit]}
+            showValuesOnTopOfBars
+          />
+        ) : (
+          <Text style={styles.noDataText}>No data available</Text>
+        )}
+      </View>
     </SafeAreaView>
   );
 };
@@ -89,18 +148,66 @@ const DataScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "flex-start",
-    paddingTop: 20,
     backgroundColor: "#f5f5f5",
   },
-  headerContainer: {
-    marginBottom: 20,
-  },
   header: {
-    fontSize: 24,
+    fontSize: 22,
     fontWeight: "bold",
-    color: "#333",
+    marginTop: 20,
+    marginBottom: 10,
+    textAlign: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginBottom: 10,
+  },
+  button: {
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 30,
+    margin: 5,
+    minWidth: 100,
+    alignItems: "center",
+  },
+  activeButton: {
+    backgroundColor: "#4e9caf",
+    elevation: 2,
+  },
+  inactiveButton: {
+    backgroundColor: "#ccc",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+  },
+  chartContainer: {
+    borderRadius: 18,
+    padding: 16,
+    marginTop: 20,
+    marginHorizontal: 16,
+    backgroundColor: "#fff",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  legend: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 8,
+  },
+  legendText: {
+    textAlign: "center",
+    fontSize: 14,
+    color: "#666666",
+  },
+  noDataText: {
+    textAlign: "center",
+    fontSize: 18,
+    color: "#999",
+    marginTop: 20,
   },
 });
 
